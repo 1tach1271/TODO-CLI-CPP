3d16c29e37b7f4286d715954447f636bccf0991f
Shashi Yadav <162320108+1tach1271@users.noreply.github.com>
2026-03-15 01:48:01 +0530

    Add files via upload

---
+++ todo.cpp
@@ -0,0 +1,70 @@
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <vector>
+
+int main(int argc, char* const argv[])
+{
+	std::string todoFilePath = std::string(getenv("HOME")) + "/.todo.db";
+	std::vector<std::string> todos;
+	std::fstream todoFileStream(todoFilePath, std::ios::in);
+	if (todoFileStream)
+	{
+		std::string todo;
+		while (std::getline(todoFileStream, todo))
+		{
+			todos.push_back(todo);
+		}
+		todoFileStream.close();
+	}
+
+	if (argc > 2 && argv[1] == std::string("add"))
+	{
+		std::string todo(argv[2]);
+		for (int i = 3; i < argc; i++)
+		{
+			todo += " " + std::string(argv[i]);
+		}
+		todos.push_back(todo);
+	}
+	else if (argc == 4 && argv[1] == std::string("swap"))
+	{
+		std::istringstream swapArg(argv[2]);
+		size_t from;
+		if (swapArg >> from && swapArg.eof() && from > 0 && from < todos.size() + 1)
+		{
+			swapArg.str(argv[3]);
+			swapArg.clear();
+			size_t to;
+			if (swapArg >> to && swapArg.eof() && to > 0 && to < todos.size() + 1)
+			{
+				std::swap(todos[from - 1], todos[to - 1]);
+			}
+		}
+	}
+	else if (argc == 3 && argv[1] == std::string("done"))
+	{
+		std::istringstream idArg(argv[2]);
+		size_t id;
+		if (idArg >> id && idArg.eof() && id > 0 && id < todos.size() + 1)
+		{
+			todos.erase(todos.begin() + id - 1);
+		}
+	}
+
+	todoFileStream.open(todoFilePath, std::ios::out | std::ios::trunc);
+	if (!todoFileStream)
+	{
+		std::cerr << "Couldn't open " + todoFilePath << std::endl;
+		return EXIT_FAILURE;
+	}
+
+	for (size_t i = 0; i < todos.size(); i++)
+	{
+		todoFileStream << todos.at(i) << std::endl;
+		std::cout << i + 1 << ": " << todos.at(i) << std::endl;
+	}
+	todoFileStream.close();
+
+	return EXIT_SUCCESS;
+}
